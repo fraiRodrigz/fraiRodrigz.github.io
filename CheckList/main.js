59675a8d12e9a5f6da287088e1363d49529688f2
fraiRodrigz <frai.rodriguezm@gmail.com>
2023-08-17 23:37:43 -0300

    Mejorando el código del reto003

--- CheckList/main.js
+++ CheckList/main.js
@@ -1,129 +1,23 @@
-let realizado1 = document.getElementById('tarea1');
-let tachado1 = document.querySelector('.tachado1');
+let contenedor = document.getElementById('tareas');
+let check = contenedor.querySelectorAll('input')
+let text = contenedor.querySelectorAll('label');
 
-realizado1.addEventListener('change', e=>{
-    if(e.target.checked === true){
-        tachado1.className = 'tachar';
-        alert('La tarea 1 fue realizada');
-    }else{
-        tachado1.className = 'tachado';
-    }
-})
-
-let realizado2 = document.getElementById('tarea2');
-let tachado2 = document.querySelector('.tachado2');
-
-realizado2.addEventListener('change', e=>{
-    if(e.target.checked === true){
-        tachado2.className = 'tachar';
-        alert('La tarea 2 fue realizada');
-    }else{
-        tachado2.className = 'tachado';
-    }
-    console.log(e)
-})
-
-let realizado3 = document.getElementById('tarea3');
-let tachado3 = document.querySelector('.tachado3');
-
-realizado3.addEventListener('change', e=>{
-    if(e.target.checked === true){
-        tachado3.className = 'tachar';
-        alert('La tarea 3 fue realizada');
-    }else{
-        tachado3.className = 'tachado';
-    }
-    console.log(e)
-})
-
-let realizado4 = document.getElementById('tarea4');
-let tachado4 = document.querySelector('.tachado4');
-
-realizado4.addEventListener('change', e=>{
-    if(e.target.checked === true){
-        tachado4.className = 'tachar';
-        alert('La tarea 4 fue realizada');
-    }else{
-        tachado4.className = 'tachado';
-    }
-    console.log(e)
-})
-
-let realizado5 = document.getElementById('tarea5');
-let tachado5 = document.querySelector('.tachado5');
-
-realizado5.addEventListener('change', e=>{
-    if(e.target.checked === true){
-        tachado5.className = 'tachar';
-        alert('La tarea 5 fue realizada');
-    }else{
-        tachado5.className = 'tachado';
-    }
-    console.log(e)
-})
-
-let realizado6 = document.getElementById('tarea6');
-let tachado6 = document.querySelector('.tachado6');
-
-realizado6.addEventListener('change', e=>{
-    if(e.target.checked === true){
-        tachado6.className = 'tachar';
-        alert('La tarea 6 fue realizada');
-    }else{
-        tachado6.className = 'tachado';
-    }
-    console.log(e)
-})
-
-let realizado7 = document.getElementById('tarea7');
-let tachado7 = document.querySelector('.tachado7');
-
-realizado7.addEventListener('change', e=>{
-    if(e.target.checked === true){
-        tachado7.className = 'tachar';
-        alert('La tarea 7 fue realizada');
-    }else{
-        tachado7.className = 'tachado';
-    }
-    console.log(e)
-})
-
-
-let realizado8 = document.getElementById('tarea8');
-let tachado8 = document.querySelector('.tachado8');
-
-realizado8.addEventListener('change', e=>{
-    if(e.target.checked === true){
-        tachado8.className = 'tachar';
-        alert('La tarea 8 fue realizada');
-    }else{
-        tachado8.className = 'tachado';
-    }
-    console.log(e)
-})
-
-let realizado9 = document.getElementById('tarea9');
-let tachado9 = document.querySelector('.tachado9');
-
-realizado9.addEventListener('change', e=>{
-    if(e.target.checked === true){
-        tachado9.className = 'tachar';
-        alert('La tarea 9 fue realizada');
-    }else{
-        tachado9.className = 'tachado';
-    }
-    console.log(e)
-})
-
-let realizado10 = document.getElementById('tarea10');
-let tachado10 = document.querySelector('.tachado10');
-
-realizado10.addEventListener('change', e=>{
-    if(e.target.checked === true){
-        tachado10.className = 'tachar';
-        alert('La tarea 10 fue realizada');
-    }else{
-        tachado10.className = 'tachado';
-    }
-    console.log(e)
-})
+check.forEach((checked, index) =>{
+    checked.addEventListener('change', e=>{
+        if(e.target.checked === true){
+            text.forEach((texto, itexto) =>{
+                if(itexto === index){
+                    texto.className = 'tachar';
+                }
+            })
+            alert(`La tarea ${index + 1 }  fue realizada`);
+        }else{
+            text.forEach((texto, itexto) =>{
+                if(itexto === index){
+                    texto.className = 'tachado';
+                }
+            })
+        }
+        console.log(e)
+    })
+} )
